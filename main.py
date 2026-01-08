8dafb8adbc339de85fa77af5e084f67bdd19ad13
Poshak K <111636844+poshak2004@users.noreply.github.com>
2026-01-08 18:46:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-# Main entry point
+# main.py
 from src.train import train_model
+from src.evaluate import evaluate_model
 
 if __name__ == "__main__":
-    model = train_model()
-    print("Model trained and ready!")
+    model, X_test, y_test = train_model()
+    evaluate_model(model, X_test, y_test)
+    print("Model trained, evaluated, and results saved.")
